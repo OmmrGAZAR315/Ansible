37b3af41cd7564e3e311a4137522ddf114317327
OmmmrGazzar3155 <omarahmedelgazzar600@gmail.com>
2024-10-27 10:52:07 +0300

    stable (missing stash)

--- test.sh
+++ test.sh
@@ -1,16 +1,20 @@
-          # shellcheck disable=SC2095
-branchName=$(echo "$GITHUB_REF" | awk -F/ '{print $3}')
-          while read -r line; do
-            # Extract the host and ansible_user from each line in the inventory
-            host=$(echo "$line" | grep -Eo '^[^#;]+(\.[^#;]+)(com|net)')
-            ansible_user=$(echo "$line" | grep -Eo 'ansible_user=[^ ]+' | cut -d'=' -f2)
-            echo host="$host" ansible_user="$ansible_user"
-           ssh -i ~/.ssh/id_ed25519 "$ansible_user@$host" "
-            cd /Ansible &&
-            git fetch --all &&
-            git checkout $branchName &&
-            git reset --hard origin/$branchName
-          " &
+# shellcheck disable=SC2095
+while read -r line; do
+  # Extract the host and ansible_user from each line in the inventory
+  host=$(echo "$line" | grep -Eo '^[^#;]+(\.[^#;]+)(com|net)')
+  ansible_user=$(echo "$line" | grep -Eo 'ansible_user=[^ ]+' | cut -d'=' -f2)
+  # shellcheck disable=SC2034
+ ssh -i ~/.ssh/id_ed25519 "$ansible_user@$host" "
+    echo $ansible_user@$host
+    cd /Ansible &&
+    currentBranchName=\$(git rev-parse --abbrev-ref HEAD) &&
+    echo \"Current branch is \$currentBranchName\" &&
+    git fetch --all &&
+    git checkout master &&
+    git reset --hard origin/master &&
+    git checkout \$currentBranchName
+    printf \"\n\"
+  " &
 done < <(grep -E '^[^\[#;]' inventory)
 wait
 
